85681047fc1a24ca7d47eedcc955231165af5c4c
saymanrabbi <mdrabbi.asm@gmail.com>
2022-09-20 12:31:35 +0600

    done

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port =  process.env.PORT ||4000;
+const userRoutes = require('./routes/user.routes.js');
+app.use(cors());
+app.use(express.json());
+app.use('/user', userRoutes );
+
 app.get('/', (req, res) => {
     res.send('Server is running');
 });
